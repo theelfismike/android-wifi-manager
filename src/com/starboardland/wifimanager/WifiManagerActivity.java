49ca1cf157eca8144176ea093420bf52019b305f
Mike Kelley <theelfismike@gmail.com>
2013-11-10 09:45:05 -0600

    Initial code commit.

---
+++ src/com/starboardland/wifimanager/WifiManagerActivity.java
@@ -0,0 +1,15 @@
+package com.starboardland.wifimanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WifiManagerActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
